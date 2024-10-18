76818fd94b43c555578fd8bdfaeb1ca9186361ff
Maksymilian Wrzesień <63720564+m-wrzesien@users.noreply.github.com>
2024-10-18 16:34:37 +0200

    fix: Remove quote in digest output

--- .github/docker-push/script.sh
+++ .github/docker-push/script.sh
@@ -10,4 +10,4 @@ fi
 docker push "$TAG"
 # docker images only prints digests when images doesn't have a tag
 imageWithoutTag=$(echo "$TAG" | awk -F: '{print $1}')
-echo "digest='$(docker image ls "$imageWithoutTag" --format '{{.Digest}}' --digests | head -n1)'" >> "$GITHUB_OUTPUT"
+echo "digest=$(docker image ls "$imageWithoutTag" --format '{{.Digest}}' --digests | head -n1)" >> "$GITHUB_OUTPUT"
